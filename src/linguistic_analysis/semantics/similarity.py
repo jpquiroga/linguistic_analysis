1decff92e014437964de4e9a8318ac82b9c49c5c
jpquiroga <jpquiroga@gmail.com>
2021-02-08 00:33:14 +0100

    Add building from gensim embeddings. Add export to GEFX format.

--- src/linguistic_analysis/semantics/similarity.py
+++ src/linguistic_analysis/semantics/similarity.py
@@ -24,6 +24,42 @@ class SemGraph():
         for i, n in enumerate(names):
             self.graph.add_node(i, name=n, s=self.__get_initial_score(i))
 
+    @classmethod
+    def build_from_gensim_embedding(cls, embedding_model: "gensim.models.base_any2vec.BaseWordEmbeddingsModel",
+                             base_words: List[Text], n_top_similar: int= 10, similarity_threshold: float= 0.2)\
+            -> "SemGraph":
+        """
+        Build a Semgraph instance from a list of base words an and a gensim embedding model.
+        :param embedding_model:
+        :param base_words: List of words to be taken as the basis for build a Semgraph.
+        :param n_top_similar: The number of most similar words to be analyzed for every base word.
+        :param similarity_threshold: Words with lower similarity than this threshold will not be added to the Semgraph.
+            Edges with lower similarity than this threshold will not be added either.
+
+        Cosine similarity is used.
+
+        :return: A Semgraph.
+        """
+        # 1. Identify words to add to the Semgraph
+        words = [w for w in base_words]
+        for w in base_words:
+            candidate_words = embedding_model.similar_by_word(w, topn=n_top_similar)
+            for w,sim in candidate_words:
+                if sim > similarity_threshold and w not in words:
+                    words.append(w)
+        words = sorted(set(words))
+        res = SemGraph(words)
+        # 2 Add similarity values as edge weights
+        for i in range(len(words)):
+            for j in range(i, len(words)):
+                w1 = words[i]
+                w2 = words[j]
+                if w1 != w2:
+                    similarity = embedding_model.similarity(w1, w2)
+                    if similarity > similarity_threshold:
+                        res.add_edge_with_names(w1, w2, similarity)
+        return res
+
     def __get_initial_score(self, index: int):
         return np.array([1.0 if j==index else 0.0 for j in range(self.dimension)])
 
@@ -74,3 +110,14 @@ class SemGraph():
         for i, s in enumerate(s1):
             res += np.linalg.norm(s1, s2[i])
         return res
+
+    def save_to_gefx(self, path:Text):
+        """
+        Save the current graph to GEXF (Graph Exchange XML Format).
+        :param path: The path to export to.
+        """
+        # numpy ndarrays cannot be saved to GEXF. Need to convert them to strings.
+        g_exp = self.graph.copy()
+        for n in g_exp.nodes:
+            g_exp.nodes[n]["s"] = str(g_exp.nodes[n]["s"])
+        nx.write_gexf(g_exp, path)
